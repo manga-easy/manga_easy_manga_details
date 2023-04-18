f455b836bd1e381d2be4f75813df1d5c9f1c9f47
Bruno Almeida de Matos <65208006+BrunoAlm@users.noreply.github.com>
2023-04-17 21:57:23 -0300

    microapp routes

--- lib/src/microapp/manga_details_microapp.dart
+++ lib/src/microapp/manga_details_microapp.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:manga_easy_manga_details/src/features/data/datasources/get_manga_chapters_qty_datasource.dart';
 import 'package:manga_easy_manga_details/src/features/data/datasources/local/get_manga_chapters_qty_datasource_imp.dart';
@@ -6,10 +7,17 @@ import 'package:manga_easy_manga_details/src/features/domain/repositories/get_ma
 import 'package:manga_easy_manga_details/src/features/domain/usecases/get_manga_chapters_qty/get_manga_chapters_qty_usecase.dart';
 import 'package:manga_easy_manga_details/src/features/domain/usecases/get_manga_chapters_qty/get_manga_chapters_qty_usecase_imp.dart';
 import 'package:manga_easy_manga_details/src/features/presenter/controllers/manga_details_controller.dart';
+import 'package:manga_easy_manga_details/src/features/presenter/ui/manga_details_page.dart';
+import 'package:manga_easy_routes/manga_easy_routes.dart';
 
-class MangaDetailsMicroApp {
+class MangaDetailsMicroApp extends MicroApp {
   GetIt getIt = GetIt.instance;
+  @override
+  Map<String, Widget> routers = {
+    MangaDetailsPage.route: const MangaDetailsPage()
+  };
 
+  @override
   void registerDependencies() {
     // datasources
     getIt.registerFactory<GetMangaChaptersQtyDataSource>(
@@ -25,7 +33,7 @@ class MangaDetailsMicroApp {
     getIt.registerFactory<GetMangaChaptersQtyUseCase>(
       () => GetMangaChaptersQtyUseCaseImp(getIt()),
     );
-    
+
     // controllers
     getIt.registerFactory<MangaDetailsController>(
       () => MangaDetailsController(getIt()),
